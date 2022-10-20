1e039e80cd8ef9dd63ec4cf4d97ebd5ea4c3fdd1
backl1n <backl1n@backl1n.tk>
2022-10-20 22:17:34 +0200

    1er version

--- clonyg.py
+++
@@ -1,7 +0,0 @@
-username = open("username.dat", "r")
-lines = username.readlines()
-# fermez le fichier après avoir lu les lignes
-username.close()
-# Itérer sur les lignes
-for line in lines:
-    print(line.strip())
--- clonyg.sh
+++ clonyg.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
-cat ascii.dat
-python clonyg.py
-#sleep 5
-#git clone 
+#ascci art signature
+if [[ "$1" = '-w' ]]; then
+    cat ascii.dat
+
+elif [[ "$1" = '-h' ]]; then
+    cat help.txt
+
+#clone debug
+elif [[ "$1" = "-d" ]]; then
+    #progress bar 
+    echo "telechargement des repos  en cours "
+    #recuperation pseudo
+    #fichier contenant les usernames
+    fichier="username.dat"
+    oldIFS=$IFS # sauvegarde du séparateur de champ
+    IFS=$'\n'   # nouveau séparateur de champ, le caractère fin de ligne
+
+    for ligne in $(<$fichier); do
+        #afficher le pseudo
+        #telecharge
+        git clone https://gitlab.com/$ligne/$2 ./projet/$2/$ligne
+    done
+
+#clone
+elif [[ "$1" = "-c" ]]; then
+    echo "telechargement des repos  en cours "
+    #recuperation pseudo
+    #fichier contenant les usernames
+    fichier="username.dat"
+    oldIFS=$IFS # sauvegarde du séparateur de champ
+    IFS=$'\n'   # nouveau séparateur de champ, le caractère fin de ligne
+
+    for ligne in $(<$fichier); do
+        #telecharge en mode silencieux
+        git clone -q https://gitlab.com/$ligne/$2 ./projet/$2/$ligne
+    
+    done
+fi
